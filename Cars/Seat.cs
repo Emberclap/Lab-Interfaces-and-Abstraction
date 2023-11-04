e2d0f380447c60b7560c1e1622d0a04412f81079
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-11-04 15:39:29 +0200

    Add project files.

---
+++ Cars/Seat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string car)
+        {
+            Model = model;
+            Car = car;
+        }
+
+        public string Model { get; set; }
+        public string Car { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+        public override string ToString()
+        {
+            return $"{this.Car} Seat {this.Model}";
+        }
+    }
+}
---
+++ Cars/Tesla.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string car, int battery)
+        {
+            Model = model;
+            Car = car;
+            Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Car { get; set; }
+        public int Battery { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start"); ;
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Car} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
